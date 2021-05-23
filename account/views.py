cc4cd139c0e0f3945eb5bed0b41ad16089dd68ab
isabel-lombardi <isabel.lombardi96@gmail.com>
2021-05-23 21:43:37 +0200

    [FEAT] Create signup and logout method

--- account/views.py
+++ account/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .serializers import UserSerializer
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import status         # for send status
+from rest_framework.views import APIView  # class view
+
+
+class SignupAPIView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)  # handle incoming json requests, get data from request
+
+        # validate the input data and confirm that all required fields are correct
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response('Correctly registered', status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutAPIView(APIView):
+
+    def get(self, request):
+        # simply delete the token to force a login
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
