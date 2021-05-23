91a92f890a0e1e8d18740631009660dcf6b740f5
isabel-lombardi <isabel.lombardi96@gmail.com>
2021-05-23 21:45:13 +0200

    [FEAT] Use and configure User model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    # will hold a One-To-One relationship with the existing User Model
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
